7bc6263afd816d2ee2ed080d880a682661210056
salva <salva@ugr.es>
2023-03-07 22:10:30 +0000

    distribuyendo

--- src_main_pub/version.F90
+++ src_main_pub/version.F90
@@ -1,10 +1,7 @@
-!  
 module  version 
 character (128), parameter :: compdate= &  
-' Tue Mar 7 20:45:39 2023 +0100 ' // & 
 ' '  
 character(128), parameter :: dataversion= & 
-'SEMBA_FDTD Version 74a16df ' // &  
-'Built on ' // & 
+'Commit on ' // & 
 trim(adjustl(compdate))  
 end module version 
