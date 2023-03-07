953e2aec49012fba59a059f4db5b3dac708729d3
salva <salva@ugr.es>
2023-03-07 22:12:53 +0000

    distribuyendo

--- src_main_pub/version.F90
+++ src_main_pub/version.F90
@@ -1,7 +1,9 @@
 module  version 
 character (128), parameter :: compdate= &  
+' Tue Mar 7 22:10:40 2023 +0000' // & 
 ' '  
 character(128), parameter :: dataversion= & 
+'UGRFDTD Version d6cb1eb ' // &  
 'Commit on ' // & 
 trim(adjustl(compdate))  
 end module version 
