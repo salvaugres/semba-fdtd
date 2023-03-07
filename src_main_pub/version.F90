0eec27313f0dc290f4ebd74417bc1158fc3792c4
salva <salva@ugr.es>
2023-03-07 22:43:09 +0000

    Distribuyendo defin

--- src_main_pub/version.F90
+++ src_main_pub/version.F90
@@ -1,9 +1,9 @@
 module  version 
 character (128), parameter :: compdate= &  
-' Tue Mar 7 22:31:17 2023 +0000' // & 
+' Tue Mar 7 22:38:36 2023 +0000' // & 
 ' '  
 character(128), parameter :: dataversion= & 
-'SEMBA-FDTD Version 656f40f ' // &  
+'SEMBA-FDTD Version c2e40eb ' // &  
 'Commit on ' // & 
 trim(adjustl(compdate))  
 end module version 
