1bd25eae8452602112f584034a963a51b6bd9cf8
Salvador Gonzalez <salva@ugr.es>
2023-03-07 20:45:31 +0100

    versionando

--- src_main_pub/version.F90
+++ src_main_pub/version.F90
@@ -1,10 +1,10 @@
 !  
 module  version 
 character (128), parameter :: compdate= &  
-' Tue Mar 7 20:38:46 2023 +0100 ' // & 
+' Tue Mar 7 20:42:39 2023 +0100 ' // & 
 ' '  
 character(128), parameter :: dataversion= & 
-'SEMBA_FDTD Version 1acadcc ' // &  
+'SEMBA_FDTD Version b265c90 ' // &  
 'Built on ' // & 
 trim(adjustl(compdate))  
 end module version 
