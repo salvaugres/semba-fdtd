192dd9bd9131d9ea8b591715d3de8c30ba31ec07
alberto <albergascon@gmail.com>
2023-12-22 15:48:45 +0100

    Refactorization of dispersives finished

--- src_mtln/src/dispersive.f90
+++ src_mtln/src/dispersive.f90
@@ -5,8 +5,6 @@ module dispersive_mod
     use rational_approximation_mod
     implicit none
     
-
-
     type :: dispersive_t
         real :: dt
         integer :: number_of_divisions, number_of_conductors, number_of_poles
@@ -42,7 +40,8 @@ module dispersive_mod
     contains
         procedure :: addTransferImpedance
         procedure :: setTransferImpedance
-        ! procedure, private :: readDirection
+        procedure, private :: isCouplingInwards
+        procedure, private :: isCouplingOutwards
         procedure, private :: computeRange
         procedure, private :: addTransferImpedanceInConductors
         procedure, private :: setTransferImpedanceInConductors
@@ -102,7 +101,19 @@ contains
 
     end subroutine
 
+    subroutine increaseOrder(this, number_of_poles)
+        class(dispersive_t) :: this
+        integer, intent(in) :: number_of_poles
+        type(dispersive_t) :: new_dispersive
 
+        new_dispersive = dispersive_t(this%number_of_conductors, number_of_poles, this%u, this%dt)
+        new_dispersive%q1(:,:,:,1:this%number_of_poles) = this%q1
+        new_dispersive%q2(:,:,:,1:this%number_of_poles) = this%q2
+        new_dispersive%q3(:,:,:,1:this%number_of_poles) = this%q3
+        call move_alloc(from=new_dispersive%q1, to=this%q1)
+        call move_alloc(from=new_dispersive%q2, to=this%q2)
+        call move_alloc(from=new_dispersive%q3, to=this%q3)
+    end subroutine
 
     function findIndex(this, position) result(res)
         class(dispersive_t) :: this
@@ -134,58 +145,40 @@ contains
         end if
     end function
 
-    subroutine increaseOrder(this, number_of_poles)
-        class(dispersive_t) :: this
-        integer, intent(in) :: number_of_poles
-        type(dispersive_t) :: new_dispersive
-
-        new_dispersive = dispersive_t(this%number_of_conductors, number_of_poles, this%u, this%dt)
-        new_dispersive%q1(:,:,:,1:this%number_of_poles) = this%q1
-        new_dispersive%q2(:,:,:,1:this%number_of_poles) = this%q2
-        new_dispersive%q3(:,:,:,1:this%number_of_poles) = this%q3
-        call move_alloc(from=new_dispersive%q1, to=this%q1)
-        call move_alloc(from=new_dispersive%q2, to=this%q2)
-        call move_alloc(from=new_dispersive%q3, to=this%q3)
-
-
-    end subroutine
 
     subroutine addDispersiveConnector(this, position, conductor, model)
         class(connector_t) :: this
         real, dimension(3), intent(in) :: position
         integer, intent(in) :: conductor
         type(fhash_tbl_t), intent(in) :: model
-        complex, allocatable, dimension(:) :: poles, residues, q1,q2,q3
-        integer :: index, connector_number_of_poles, i
+        integer :: index
         type(pol_res_t) :: connector
+
         index = this%findIndex(position)
         if (.not.this%positionIsEmpty(index, conductor))then
             error stop 'Dispersive connector already in conductor at position'
         end if
 
         connector = pol_res_t(model, this%dt)
-
-        ! poles = this%readPoles(model)
-        ! residues = this%readResidues(model)
-        
-        ! q1 = - (residues/poles) * (1.0-(exp(poles*this%dt)-1.0)/(poles*this%dt))
-        ! q2 =   (residues/poles) * (1.0/(poles*this%dt) + exp(poles*this%dt)*(1.0-1.0/(poles*this%dt)))
-        ! q3 =   exp(poles*this%dt)
-
         if (connector%number_of_poles > this%number_of_poles) call this%increaseOrder(connector%number_of_poles)
-        
         call this%addDispersiveConnectorInConductor(index, conductor, connector)
-        ! this%q1(index, conductor, conductor,:) = connector%q1(:)
-        ! this%q2(index, conductor, conductor,:) = connector%q2(:)
-        ! this%q3(index, conductor, conductor,:) = connector%q3(:)
- 
-        ! this%d(index, conductor, conductor) = connector%r
-        ! this%e(index, conductor, conductor) = connector%l
-        
+       
         this%q1_sum = sumQComponents(this%q1)
         this%q2_sum = sumQComponents(this%q2)
     end subroutine
 
+    subroutine addDispersiveConnectorInConductor(this, index, conductor, connector)
+        class(connector_t) :: this
+        integer, intent(in) :: index, conductor
+        type(pol_res_t), intent(in) :: connector
+
+        this%d(index, conductor, conductor) = this%d(index, conductor, conductor) + connector%r
+        this%e(index, conductor, conductor) = this%e(index, conductor, conductor) + connector%l
+
+        this%q1(index, conductor, conductor,:) = this%q1(index, conductor, conductor,:) - connector%q1(:)
+        this%q2(index, conductor, conductor,:) = this%q2(index, conductor, conductor,:) - connector%q2(:)
+        this%q3(index, conductor, conductor,:) = this%q3(index, conductor, conductor,:) - connector%q3(:)
+    end subroutine
 
     function transferImpendaceCtor(number_of_conductors, number_of_poles, u, dt) result(res)
         type(transfer_impedance_t) :: res
@@ -195,6 +188,22 @@ contains
         res%dispersive_t = dispersiveCtor(number_of_conductors, number_of_poles, u, dt)
     end function 
 
+    function isCouplingInwards(this, direction) result(res)
+        class(transfer_impedance_t) :: this
+        character(len=*), intent(in) :: direction
+        logical :: res
+        res = .false.
+        if (direction == "inwards" .or. direction == "both") res = .true.
+    end function
+
+    function isCouplingOutWards(this, direction) result(res)
+        class(transfer_impedance_t) :: this
+        character(len=*), intent(in) :: direction
+        logical :: res
+        res = .false.
+        if (direction == "outwards" .or. direction == "both") res = .true.
+    end function
+
     subroutine addTransferImpedance(this, levels, out_level, out_level_conductors, &
                                                   in_level, in_level_conductors, &
                                                   model)
@@ -202,22 +211,11 @@ contains
         integer, intent(in) :: out_level, in_level
         integer, dimension(:), intent(in) :: out_level_conductors, in_level_conductors
         type(fhash_tbl_t), intent(in) :: model, levels
-        complex, allocatable, dimension(:) :: poles, residues, q1,q2,q3
-        character(len=:), allocatable :: direction
-        integer :: connector_number_of_poles, i, j
+        integer :: i, j
         integer, dimension(:), allocatable :: range_in, range_out
         type(pol_res_t) :: connector
 
         connector = pol_res_t(model, this%dt)
-
-        ! poles = this%readPoles(model)
-        ! residues = this%readResidues(model)
-        ! direction = this%readDirection(model)
-        ! connector_number_of_poles = size(connector%number_of_poles)
-        ! q1 =   (residues/poles) * (1.0-(exp(poles*this%dt)-1.0)/(poles*this%dt))
-        ! q2 = - (residues/poles) * (1.0/(poles*this%dt) + exp(poles*this%dt)*(1.0-1.0/(poles*this%dt)))
-        ! q3 = - exp(poles*this%dt)
-
         if (connector%number_of_poles > this%number_of_poles) call this%increaseOrder(connector%number_of_poles)
 
         !todo compute ranges
@@ -225,31 +223,16 @@ contains
         range_out = this%computeRange(out_level, out_level_conductors)        
         do i = 1, size(range_out)
             do j = 1, size(range_in)
-                if (direction == "inwards" .or. direction == "both") then
+                if (this%isCouplingInwards(connector%direction)) then
                     call this%addTransferImpedanceInConductors(range_in(j), range_out(i), connector)
-
-                    ! this%q1(:,range_in(j), range_out(i),:) = this%q1(:,range_in(j), range_out(i),:) + connector%q1(:)
-                    ! this%q2(:,range_in(j), range_out(i),:) = this%q2(:,range_in(j), range_out(i),:) + connector%q2(:)
-                    ! this%q3(:,range_in(j), range_out(i),:) = this%q3(:,range_in(j), range_out(i),:) + connector%q3(:)
-            
-                    ! this%d(:, range_in(j), range_out(i)) = this%d(:, range_in(j), range_out(i)) - connector%r
-                    ! this%e(:, range_in(j), range_out(i)) = this%e(:, range_in(j), range_out(i)) - connector%l
-                else if (direction == "outwards" .or. direction == "both") then
+                else if (this%isCouplingOutwards(connector%direction)) then
                     call this%addTransferImpedanceInConductors(range_out(i), range_in(j), connector)
-                    ! this%q1(:,range_out(i), range_in(j),:) = this%q1(:,range_out(i), range_in(j),:) + connector%q1(:)
-                    ! this%q2(:,range_out(i), range_in(j),:) = this%q2(:,range_out(i), range_in(j),:) + connector%q2(:)
-                    ! this%q3(:,range_out(i), range_in(j),:) = this%q3(:,range_out(i), range_in(j),:) + connector%q3(:)
-
-                    ! this%d(:, range_out(i), range_in(j)) = this%d(:, range_out(i), range_in(j)) - connector%r
-                    ! this%e(:, range_out(i), range_in(j)) = this%e(:, range_out(i), range_in(j)) - connector%l
                 end if
             end do
         end do
 
         this%q1_sum = sumQComponents(this%q1)
         this%q2_sum = sumQComponents(this%q2)
-
-        
     end subroutine
     
     function computeRange(this, level, conductors) result(res)
@@ -269,20 +252,12 @@ contains
         integer, intent(in) :: index
         type(fhash_tbl_t), intent(in) :: model, levels
 
-        ! complex, allocatable, dimension(:) :: poles, residues, q1,q2,q3
-        ! character(len=:), allocatable :: direction
         integer :: i, j
         integer, dimension(:), allocatable :: range_in, range_out
 
         type(pol_res_t) :: connector
 
-        ! poles = this%readPoles(model)
-        ! residues = this%readResidues(model)
-        ! direction = this%readDirection(model)
-        ! q1 =   (residues/poles) * (1.0-(exp(poles*this%dt)-1.0)/(poles*this%dt))
-        ! q2 = - (residues/poles) * (1.0/(poles*this%dt) + exp(poles*this%dt)*(1.0-1.0/(poles*this%dt)))
-        ! q3 = - exp(poles*this%dt)
-
+        connector = pol_res_t(model, this%dt)
         if (connector%number_of_poles > this%number_of_poles) call this%increaseOrder(connector%number_of_poles)
 
         !todo compute ranges
@@ -290,22 +265,10 @@ contains
         range_out = this%computeRange(out_level, out_level_conductors)        
         do i = 1, size(range_out)
             do j = 1, size(range_in)
-                if (connector%direction == "inwards" .or. connector%direction == "both") then
+                if (this%isCouplingInwards(connector%direction)) then
                     call this%setTransferImpedanceInConductors(index, range_in(j), range_out(i), connector)
-                    ! this%q1(index,range_in(j), range_out(i),:) = connector%q1(:)
-                    ! this%q2(index,range_in(j), range_out(i),:) = connector%q2(:)
-                    ! this%q3(index,range_in(j), range_out(i),:) = connector%q3(:)
-            
-                    ! this%d(index, range_in(j), range_out(i)) = - connector%r
-                    ! this%e(index, range_in(j), range_out(i)) = - connector%l
-                else if (connector%direction == "outwards" .or. connector%direction == "both") then
+                else if (this%isCouplingOutwards(connector%direction)) then
                     call this%setTransferImpedanceInConductors(index, range_out(i), range_in(j), connector)
-                    ! this%q1(index,range_out(i), range_in(j),:) = connector%q1(:)
-                    ! this%q2(index,range_out(i), range_in(j),:) = connector%q2(:)
-                    ! this%q3(index,range_out(i), range_in(j),:) = connector%q3(:)
-
-                    ! this%d(index, range_out(i), range_in(j)) = - connector%r
-                    ! this%e(index, range_out(i), range_in(j)) = - connector%l
                 end if
             end do
         end do
@@ -319,42 +282,30 @@ contains
         integer, intent(in) :: index, conductor_1, conductor_2
         type(pol_res_t), intent(in) :: connector
 
+        this%d(index, conductor_1, conductor_2) = - connector%r
+        this%e(index, conductor_1, conductor_2) = - connector%l
+
         this%q1(index, conductor_1, conductor_2,:) = connector%q1(:)
         this%q2(index, conductor_1, conductor_2,:) = connector%q2(:)
         this%q3(index, conductor_1, conductor_2,:) = connector%q3(:)
-
-        this%d(index, conductor_1, conductor_2) = - connector%r
-        this%e(index, conductor_1, conductor_2) = - connector%l
     end subroutine
 
-    subroutine addTransferImpedanceInConductors(this, index_1, index_2, connector)
+    subroutine addTransferImpedanceInConductors(this, conductor_1, conductor_2, connector)
         class(transfer_impedance_t) :: this
-        integer, intent(in) :: index_1, index_2
+        integer, intent(in) :: conductor_1, conductor_2
         type(pol_res_t), intent(in) :: connector
         integer :: i
 
-        do i = 1, this%number_of_divisions
-            this%q1(i,index_1, index_2,:) = this%q1(i,index_1, index_2,:) + connector%q1(:)
-            this%q2(i,index_1, index_2,:) = this%q2(i,index_1, index_2,:) + connector%q2(:)
-            this%q3(i,index_1, index_2,:) = this%q3(i,index_1, index_2,:) + connector%q3(:)
+        this%d(:, conductor_1, conductor_2) = this%d(:, conductor_1, conductor_2) - connector%r
+        this%e(:, conductor_1, conductor_2) = this%e(:, conductor_1, conductor_2) - connector%l
 
-            this%d(i, index_1, index_2) = this%d(i, index_1, index_2) - connector%r
-            this%e(i, index_1, index_2) = this%e(i, index_1, index_2) - connector%l
+        do i = 1, this%number_of_divisions
+            this%q1(i,conductor_1, conductor_2,:) = this%q1(i,conductor_1, conductor_2,:) + connector%q1(:)
+            this%q2(i,conductor_1, conductor_2,:) = this%q2(i,conductor_1, conductor_2,:) + connector%q2(:)
+            this%q3(i,conductor_1, conductor_2,:) = this%q3(i,conductor_1, conductor_2,:) + connector%q3(:)
         end do
     end subroutine
 
-    subroutine addDispersiveConnectorInConductor(this, index, conductor, connector)
-        class(connector_t) :: this
-        integer, intent(in) :: index, conductor
-        type(pol_res_t), intent(in) :: connector
-
-        this%q1(index, conductor, conductor,:) = connector%q1(:)
-        this%q2(index, conductor, conductor,:) = connector%q2(:)
-        this%q3(index, conductor, conductor,:) = connector%q3(:)
-
-        this%d(index, conductor, conductor) = - connector%r
-        this%e(index, conductor, conductor) = - connector%l
-    end subroutine
 
 
 end module dispersive_mod
