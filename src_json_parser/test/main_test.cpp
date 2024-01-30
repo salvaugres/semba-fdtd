ac5702f837e73704bea5baa7bf6c507541186d3e
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2024-01-30 10:18:13 +0100

    Converting to googletest

--- src_json_parser/test/main_test.cpp
+++ src_json_parser/test/main_test.cpp
@@ -1,24 +1,23 @@
 #include <gtest/gtest.h>
 
-//extern "C" int test_cells();
-//extern "C" int test_fhash();
+extern "C" int test_fhash();
+
+extern "C" int test_cells();
+
+extern "C" int test_mesh_add_get();
+extern "C" int test_mesh_node_to_pixel();
+extern "C" int test_mesh_polyline_to_linel();
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
-TEST(test, test)
-{
-    EXPECT_TRUE(true);
-}
+TEST(fhash, basic_operation) {EXPECT_EQ(0, test_fhash()); }
+// TEST(fhahs, idchildtable) {EXPECT_EQ(0, test_idchil)}
 
-//TEST(Cells, cells)
-//{
-//	EXPECT_EQ(test_cells(), 0);
-//}
-//
-//TEST(fhash, fhash)
-//{
-//	EXPECT_EQ(test_cells(), 0);
-//}
+TEST(mesh, cells)             { EXPECT_EQ(0, test_cells()); }
+TEST(mesh, add_get)           { EXPECT_EQ(0, test_mesh_add_get()); }
+TEST(mesh, node_to_pixel)     { EXPECT_EQ(0, test_mesh_node_to_pixel()); }
+TEST(mesh, polyline_to_linel) { EXPECT_EQ(0, test_mesh_polyline_to_linel()); }
