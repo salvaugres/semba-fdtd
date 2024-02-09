28b80c8b3834d7b4867bf566a89c35be09c498d3
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2024-02-09 10:22:39 +0100

    Configuring actions

--- .github/workflows/run_tests_windows.bat
+++ .github/workflows/run_tests_windows.bat
@@ -19,4 +19,4 @@ IF "%VS_VER%"=="2017_build_tools" (
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
 
-build\bin\semba_tests.exe
+build\bin\smbjson_tests.exe
