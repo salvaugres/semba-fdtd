1694a6cabec9fa4b1044fca519a74068ad01707e
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2024-03-23 12:39:13 +0100

    Removes current label for from nodal source field.

--- test/smbjson/test_read_currentinjection.F90
+++ test/smbjson/test_read_currentinjection.F90
@@ -91,10 +91,10 @@ contains
       expected%nodSrc%n_C2P_max = 1
       allocate(expected%nodSrc%NodalSource(1))
       expected%nodSrc%NodalSource(1)%nombre = "gauss.exc"
-      expected%nodSrc%NodalSource(1)%isElec = .false.
+      expected%nodSrc%NodalSource(1)%isElec = .true.
       expected%nodSrc%NodalSource(1)%isMagnet = .false.
-      expected%nodSrc%NodalSource(1)%isCurrent = .true.
-      expected%nodSrc%NodalSource(1)%isField = .false.
+      expected%nodSrc%NodalSource(1)%isCurrent = .false.
+      expected%nodSrc%NodalSource(1)%isField = .true.
       expected%nodSrc%NodalSource(1)%isInitialValue = .false.
       allocate(expected%nodSrc%NodalSource(1)%c1P(0))
       allocate(expected%nodSrc%NodalSource(1)%c2P(1))
